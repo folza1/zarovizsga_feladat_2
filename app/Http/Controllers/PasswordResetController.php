a4fd6cdf57cb978543d32db6b5ef64ec67bd7ec7
folza1 <fodorzoltan317@gmail.com>
2024-01-11 17:41:45 +0100

    8.Token_problem

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -6,6 +6,7 @@ use App\Mail\ResetPasswordMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class PasswordResetController extends Controller
 {
@@ -27,10 +28,13 @@ class PasswordResetController extends Controller
             return redirect()->back()->withErrors(['email' => 'Az adott e-mailcím nem található a rendszerünkben.']);
         }
 
+        $token = Str::random(60); // 60 karakter hosszú egyedi token
+
         // Az e-mailcím megtalálható, küldj e-mailt
         $details = [
             'title' => 'Mail from Laravel',
             'body' => 'This is for testing email using SMTP',
+            'token' => $token,
         ];
 
         Mail::to($request->email)->send(new ResetPasswordMail($details));
--- resources/views/reset_password_email.blade.php
+++ resources/views/reset_password_email.blade.php
@@ -1,6 +1,4 @@
 <h1>Elfelejtett jelszó visszaállítás</h1>
 <h3>Az alábbi linkre kattintva új jelszót adhat meg!</h3>
-<a href="http://localhost:8000/login">localhost:8000/password_reset</a>
-{{--<p>{{ $details['title'] }}</p>--}}
-{{--<p>{{ $details['body'] }}</p>--}}
+<a href="http://localhost:8000/jelszovisszaallitas/{{$details['token']}}">Jelszóvisszaállítás link!!!</a>
 

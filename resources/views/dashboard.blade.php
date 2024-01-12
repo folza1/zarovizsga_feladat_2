150412b3e6a1fc6ccbf3692399cb8918f2ed55f3
folza1 <fodorzoltan317@gmail.com>
2024-01-12 12:13:36 +0100

    10.Project_To_Proofread

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit">Kijelentkezés</button>
-</form>
+<x-layout>
+    <h1 class="text-3xl text-center font-bold my-6">Be vagy jelentkezve kedves  {{ Auth::user()->name }}!</h1>
+    <form action="{{ route('logout') }}" method="POST" class="flex justify-center">
+        @csrf
+        <button class="text-3xl text-center font-bold my-6 border bg-red-400
+         hover:bg-red-200 p-2" type="submit"
+        >Kijelentkezés</button>
+    </form>
+</x-layout>
--- resources/views/reset_password_email.blade.php
+++ resources/views/reset_password_email.blade.php
@@ -1,3 +1,10 @@
-<h1>Elfelejtett jelszó visszaállítás</h1>
-<h3>Az alábbi linkre kattintva új jelszót adhat meg!</h3>
-<a href="http://localhost:8000/jelszo/reset/{{$details['token']}}">Jelszóvisszaállítás link!!!</a>
+<x-layout>
+    <div class="border bg-blue-300">
+        <h1 class="my-5 text-2xl text-center font-bold">Elfelejtett jelszó visszaállítás</h1>
+        <h3 class="my-4 text-xl text-center font-bold">Az alábbi linkre kattintva új jelszót adhat meg!</h3>
+        <a href="http://localhost:8000/jelszo/reset/{{$details['token']}}"
+           style="display: block; text-align: center; margin-left: auto; margin-right: auto;"
+        class="my-4"
+        > Jelszóvisszaállítás link!!!</a>
+    </div>
+</x-layout>
